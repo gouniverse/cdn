df4d205eda96d97fad7d36b02ee2f9b43f299f66
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-10 19:43:21 +0100

    Update and rename BootstrapIconsCss183.go to BootstrapIconsCss191.go

--- BootstrapIconsCss183.go
+++
@@ -1,5 +0,0 @@
-package cdn
-
-func BootstrapIconsCss183() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css"
-}
---
+++ BootstrapIconsCss191.go
@@ -0,0 +1,5 @@
+package cdn
+
+func BootstrapIconsCss191() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+}
