c6fa399ff3a6541c306bcab2f635b994b3d8fba8
Milan Lesichkov <lesichkovm@gmail.com>
2023-04-25 08:02:14 +0100

    Update BootstrapCss523.go

--- BootstrapCss523.go
+++ BootstrapCss523.go
@@ -1,2 +1,9 @@
-https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css
-https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js
+package cdn
+
+func BootstrapCss523() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+}
+
+func BootstrapJs523() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+}
