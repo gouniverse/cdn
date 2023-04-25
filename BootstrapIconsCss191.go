1650e3b06d86040c0e009f1f64a8d74db1b6c424
Milan Lesichkov <lesichkovm@gmail.com>
2023-04-25 07:13:40 +0000

    Update

--- BootstrapIconsCss191.go
+++ BootstrapIconsCss191.go
@@ -3,7 +3,3 @@ package cdn
 func BootstrapIconsCss191() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
 }
-
-func BootstrapIconsCss1102() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
-}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gouniverse/cdn
+
+go 1.20
