dc2541ea8cffb4d0ab4a3c7edcfdeaeebc406e71
Milan Lesichkov <lesichkovm@gmail.com>
2023-05-02 22:11:51 +0000

    Updates

---
+++ Jquery.go
@@ -0,0 +1,5 @@
+package cdn
+
+func Jquery_3_6_4() string {
+	return "https://code.jquery.com/jquery-3.6.4.min.js"
+}
---
+++ JqueryDataTables.go
@@ -0,0 +1,9 @@
+package cdn
+
+func JqueryDataTablesCss_1_13_4() string {
+	return "https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css"
+}
+
+func JqueryDataTablesJs_1_13_4() string {
+	return "https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"
+}
