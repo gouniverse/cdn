3eb857d33fa538285e1a6d1b501f75e4a5888179
Milan Lesichkov <lesichkovm@gmail.com>
2023-07-15 07:33:18 +0000

    Updates

--- Bootstrap.go
+++ Bootstrap.go
@@ -1,12 +1,6 @@
 package cdn
 
-func BootstrapCss_5_2_3() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-}
-
-func BootstrapJs_5_2_3() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-}
+// ======================= version 5.2.3 ======================= //
 
 func BootstrapCss_5_3_0() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
@@ -14,4 +8,114 @@ func BootstrapCss_5_3_0() string {
 
 func BootstrapJs_5_3_0() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+}
+
+func BootstrapCeruleanCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/cerulean/bootstrap.min.css"
+}
+
+func BootstrapCosmoCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/cosmo/bootstrap.min.css"
+}
+
+func BootstrapCyborgCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/cyborg/bootstrap.min.css"
+}
+
+func BootstrapDarklyCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/darkly/bootstrap.min.css"
+}
+
+func BootstrapFlatlyCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/flatly/bootstrap.min.css"
+}
+
+func BootstrapJournalCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/journal/bootstrap.min.css"
+}
+
+func BootstrapLiteraCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/litera/bootstrap.min.css"
+}
+
+func BootstrapLumenCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/lumen/bootstrap.min.css"
+}
+
+func BootstrapLuxCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/lux/bootstrap.min.css"
+}
+
+func BootstrapMateriaCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/matera/bootstrap.min.css"
+}
+
+func BootstrapMorphCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/morph/bootstrap.min.css"
+}
+
+func BootstrapMintyCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/minty/bootstrap.min.css"
+}
+
+func BootstrapPulseCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/pulse/bootstrap.min.css"
+}
+
+func BootstrapQuartzCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/quartz/bootstrap.min.css"
+}
+
+func BootstrapSandstoneCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/sandstone/bootstrap.min.css"
+}
+
+func BootstrapSimplexCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/simplex/bootstrap.min.css"
+}
+
+func BootstrapSketchyCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/sketchy/bootstrap.min.css"
+}
+
+func BootstrapSlateCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/slate/bootstrap.min.css"
+}
+
+func BootstrapSolarCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/solar/bootstrap.min.css"
+}
+
+func BootstrapSpacelabCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/space/bootstrap.min.css"
+}
+
+func BootstrapSuperheroCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/superhero/bootstrap.min.css"
+}
+
+func BootstrapUnitedCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/united/bootstrap.min.css"
+}
+
+func BootstrapVaporCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/vapor/bootstrap.min.css"
+}
+
+func BootstrapZephyrCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/zephyr/bootstrap.min.css"
+}
+
+func BootstrapYetiCss_5_3_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/yeti/bootstrap.min.css"
+}
+
+// ======================= version 5.2.3 ======================= //
+
+func BootstrapCss_5_2_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+}
+
+func BootstrapJs_5_2_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
 }
