e910b1c1671d7efe6a8915f3d542feb949772502
Milan Lesichkov <lesichkovm@gmail.com>
2023-10-25 22:39:30 +0100

    Update BootstrapIcons.go

--- BootstrapIcons.go
+++ BootstrapIcons.go
@@ -1,13 +1,17 @@
 package cdn
 
-func BootstrapIconsCss_1_9_1() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+func BootstrapIconsCss_1_11_0() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"
+}
+
+func BootstrapIconsCss_1_10_5() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
 }
 
 func BootstrapIconsCss_1_10_2() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
 }
 
-func BootstrapIconsCss_1_10_5() string {
-	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+func BootstrapIconsCss_1_9_1() string {
+	return "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
 }
