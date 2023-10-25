e62b2756fcbd789927661015b4a7c2837261b83e
Milan Lesichkov <lesichkovm@gmail.com>
2023-10-25 22:43:28 +0100

    Update Bootstrap_test.go

--- Bootstrap_test.go
+++ Bootstrap_test.go
@@ -5,6 +5,22 @@ import (
 	"testing"
 )
 
+func TestBootstrapCss_5_3_2(t *testing.T) {
+	output := BootstrapCss_5_3_2()
+	expected := "bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
+
+func TestBootstrapJs_5_3_2(t *testing.T) {
+	output := BootstrapJs_5_3_2()
+	expected := "bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
+
 func TestBootstrapCss_5_3_1(t *testing.T) {
 	output := BootstrapCss_5_3_1()
 	expected := "bootstrap@5.3.1/dist/css/bootstrap.min.css"
