a438ae81486c079806e99ae4441ee26206e14adf
Milan Lesichkov <lesichkovm@gmail.com>
2024-01-02 23:10:28 +0000

    Update JqTree_test.go

--- JqTree_test.go
+++ JqTree_test.go
@@ -5,6 +5,22 @@ import (
 	"testing"
 )
 
+func TestJqTreeCss_1_8_0(t *testing.T) {
+	output := JqTreeCss_1_8_0()
+	expected := "jqtree/1.8.0/jqtree.min.css"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
+
+func TestJqTreeJs_1_8_0(t *testing.T) {
+	output := JqTreeJs_1_8_0()
+	expected := "jqtree/1.8.0/tree.jquery.min.js"
+	if !strings.Contains(output, expected) {
+		t.Error("Does not contain '" + expected + "', Output:" + output)
+	}
+}
+
 func TestJqTreeCss_1_7_0(t *testing.T) {
 	output := JqTreeCss_1_7_0()
 	expected := "jqtree/1.7.0/jqtree.min.css"
