83782350c0d61d62d53be765a3d85e3ff726b104
Milan Lesichkov <lesichkovm@gmail.com>
2024-07-06 08:09:06 +0100

    Update Bootstrap.go

--- Bootstrap.go
+++ Bootstrap.go
@@ -13,6 +13,106 @@ func BootstrapJs_5_3_3() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 }
 
+func BootstrapCeruleanCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cerulean/bootstrap.min.css"
+}
+
+func BootstrapCosmoCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cosmo/bootstrap.min.css"
+}
+
+func BootstrapCyborgCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cyborg/bootstrap.min.css"
+}
+
+func BootstrapDarklyCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/darkly/bootstrap.min.css"
+}
+
+func BootstrapFlatlyCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/flatly/bootstrap.min.css"
+}
+
+func BootstrapJournalCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/journal/bootstrap.min.css"
+}
+
+func BootstrapLiteraCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/litera/bootstrap.min.css"
+}
+
+func BootstrapLumenCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lumen/bootstrap.min.css"
+}
+
+func BootstrapLuxCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css"
+}
+
+func BootstrapMateriaCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/materia/bootstrap.min.css"
+}
+
+func BootstrapMorphCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/morph/bootstrap.min.css"
+}
+
+func BootstrapMintyCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/minty/bootstrap.min.css"
+}
+
+func BootstrapPulseCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/pulse/bootstrap.min.css"
+}
+
+func BootstrapQuartzCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/quartz/bootstrap.min.css"
+}
+
+func BootstrapSandstoneCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/sandstone/bootstrap.min.css"
+}
+
+func BootstrapSimplexCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/simplex/bootstrap.min.css"
+}
+
+func BootstrapSketchyCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/sketchy/bootstrap.min.css"
+}
+
+func BootstrapSlateCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/slate/bootstrap.min.css"
+}
+
+func BootstrapSolarCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/solar/bootstrap.min.css"
+}
+
+func BootstrapSpacelabCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/space/bootstrap.min.css"
+}
+
+func BootstrapSuperheroCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/superhero/bootstrap.min.css"
+}
+
+func BootstrapUnitedCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/united/bootstrap.min.css"
+}
+
+func BootstrapVaporCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/vapor/bootstrap.min.css"
+}
+
+func BootstrapZephyrCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/zephyr/bootstrap.min.css"
+}
+
+func BootstrapYetiCss_5_3_3() string {
+	return "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/yeti/bootstrap.min.css"
+}
+
 // ======================= version 5.3.2 ======================= //
 func BootstrapCss_5_3_2() string {
 	return "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
